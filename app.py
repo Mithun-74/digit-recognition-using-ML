ddd4fd46e99a6e9ec04ddd26e1a98d7575954a10
Mithun-74 <mithun.coder22@gmail.com>
2025-09-29 21:44:20 +0530

    port changed

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+import os
 import base64
 import io
 import numpy as np
@@ -45,5 +46,8 @@ def predict():
         "probabilities": predictions.tolist()
     })
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render injects $PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
+
